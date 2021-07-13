335c546cdcc4d925fb249e95adadf27556c0b1c6
LeeKyuSung <kslksks@kaist.ac.kr>
2021-07-13 17:04:30 +0900

    project name 변경. 작업내용 중간 commit. sqlite로 바꾸는거 잘 안됨. 그냥 java 객체로 저장하고 읽어오는 형태로 바꿀 예정.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,19 @@
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+import com.healthmarketscience.jackcess.Database;
+
 public class Test {
-	public static void main(String[] args) {
-		System.out.println("Hello");
+	public static void main(String[] args) throws Exception {
+		Class.forName("org.sqlite.JDBC");
+		// lib 폴더에 jdbc jar를 classpath에 추가해줘야함.
+
+		String mdb = "mdb/callOrder.mdb";
+		String sqlite = "tmp.sqlite";
+		
+		final AccessExporter exporter = new AccessExporter(Database.open(new File(mdb), true));
+		final Connection jdbc = DriverManager.getConnection("jdbc:sqlite:" + sqlite);
+		exporter.export(jdbc);
 	}
 }
