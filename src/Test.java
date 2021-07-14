867e1aaff8fb64bd5d722ee1550b835695b21577
LeeKyuSung <kslksks@kaist.ac.kr>
2021-07-14 17:01:33 +0900

    ucanaccess로 mdb파일 접속 성공.

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,43 @@
-import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
-
-import com.healthmarketscience.jackcess.Database;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Test {
 	public static void main(String[] args) throws Exception {
-		Class.forName("org.sqlite.JDBC");
-		// lib 폴더에 jdbc jar를 classpath에 추가해줘야함.
+		// TODO list
+		// mdb 파일들을 모두 Object로 바꿈
+		// Object들을 모두 바이트로 바꾸고 암호화해서 저장
 
-		String mdb = "mdb/callOrder.mdb";
-		String sqlite = "tmp.sqlite";
-		
-		final AccessExporter exporter = new AccessExporter(Database.open(new File(mdb), true));
-		final Connection jdbc = DriverManager.getConnection("jdbc:sqlite:" + sqlite);
-		exporter.export(jdbc);
+		// Android에서 모두 복호화해서 Object로 바꿈
+		// 필요한거 빼고 모두 폐기
+		// 필요한 내용들만 메모리에 들고 있기
+
+		getMDB();
+	}
+
+	public static void getMDB() {
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+		String location = "mdb/iriwaamain.mdb";
+		String url = "jdbc:ucanaccess://" + location;
+		String query = "SELECT * FROM customer";
+		try {
+			conn = DriverManager.getConnection(url);
+			stmt = conn.createStatement();
+			rs = stmt.executeQuery(query);
+			while (rs.next()) {
+				String first = rs.getString(1);
+				System.out.println(first);
+			}
+			rs.close();
+			stmt.close();
+			conn.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
 	}
 }
